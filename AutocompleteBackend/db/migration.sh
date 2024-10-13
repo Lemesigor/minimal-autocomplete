109bcebc4c5c51e74fdb2ea896073e98c4e231f6
Igor Lemes <igor.lemes@live.com>
2024-10-13 17:42:56 -0300

    add dockerfile to run backend app

--- AutocompleteBackend/db/migration.sh
+++ AutocompleteBackend/db/migration.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
+root_dir="$(realpath "$(dirname "$0")/..")"
+db_dir="$root_dir/db"
 db_url="jdbc:mysql://localhost:3306/autocomplete?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true"
-db_properties="./liquibase.properties"
+db_properties="$db_dir/liquibase.properties"
+err=output.err
 
+
+cd $db_dir
+
+
+echo "Running liquibase update"
 ./liquibase-3.5.3-bin/liquibase --url="$db_url" --defaultsFile=$db_properties update
 
-if ! grep -Fxq "Liquibase Update Successful"; then
-  exit 1
-fi
+cat $err
+
+echo "Liquibase Update finished"
